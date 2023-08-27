73612e69e3a792719a6ecff501875d0d505a65b0
Ryan Valera <valeraryan@gmail.com>
2023-08-26 22:10:00 -0700

    Created a 'global' object and router function to test what page the application is on.

--- src/index.js
+++ src/index.js
@@ -5,4 +5,29 @@ import '@fortawesome/fontawesome-free/js/brands';
 import './styles/style.css';
 
 // Create a Router to navigate through html pages
+const global = {
+    currentPage: window.location.pathname
+}
 
+function init() {
+    switch (global.currentPage) {
+        case '/':
+        case '/index.html':
+            console.log('Home')
+            break;
+        case '/shows.html':
+            console.log('Shows')
+            break;
+        case '/movie-details.html':
+            console.log('Movie Details');
+            break;
+        case '/tv-details.html':
+            console.log('TV Details')
+            break;
+        case '/search.html':
+            console.log('Search')
+            break;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init)

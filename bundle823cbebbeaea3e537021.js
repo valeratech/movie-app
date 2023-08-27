db27f46c6fc372c9343b75b6bf2df29a21128c5b
Ryan Valera <valeraryan@gmail.com>
2023-08-26 22:10:00 -0700

    Created a 'global' object and router function to test what page the application is on.

--- bundle823cbebbeaea3e537021.js
+++ bundle823cbebbeaea3e537021.js
@@ -7072,10 +7072,35 @@ __webpack_require__.r(__webpack_exports__);
 
 
 // Create a Router to navigate through html pages
+const global = {
+    currentPage: window.location.pathname
+}
 
+function init() {
+    switch (global.currentPage) {
+        case '/':
+        case '/index.html':
+            console.log('Home')
+            break;
+        case '/shows.html':
+            console.log('Shows')
+            break;
+        case '/movie-details.html':
+            console.log('Movie Details');
+            break;
+        case '/tv-details.html':
+            console.log('TV Details')
+            break;
+        case '/search.html':
+            console.log('Search')
+            break;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init)
 
 })();
 
 /******/ })()
 ;
-//# sourceMappingURL=bundle832530af5b291e68bcde.js.map
+//# sourceMappingURL=bundle823cbebbeaea3e537021.js.map

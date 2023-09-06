0768372dce4147328c372ef8ead219cc4a471bd2
Ryan Valera <valeraryan@gmail.com>
2023-09-05 21:34:03 -0700

    Modified style.css: Added border/hover around tiles when highlighted, changed body and header color, increased logo font-size, removed bottom-border around show-info list item in details-pages.

--- src/modules/SearchMedia.js
+++ src/modules/SearchMedia.js
@@ -21,7 +21,6 @@ async function searchMedia(searchState) {
     if(currentState.search.term !== '') {
         // Deconstruct the required keys from the search API results which will be used for display and pagination
         const {results, total_pages, page} = await searchAPIData(currentState.search.type, currentState.search.term);
-        console.log('THIS', type);
 
         // Use a ternary condition to render the active html page with required results
         const mediaContainer = document.getElementById(

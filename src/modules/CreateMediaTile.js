3c797e9a5aad2fa6308b2805c35b53ff7f45bb9b
Ryan Valera <valeraryan@gmail.com>
2023-09-07 19:51:21 -0700

    Changed the styling of the card class in style.css as well as the 2nd color of the header gradiant.

--- src/modules/CreateMediaTile.js
+++ src/modules/CreateMediaTile.js
@@ -2,17 +2,18 @@ import noImage from '../assets/no-image.jpg'
 
 function createMediaTileContainer(mediaData, type) {
     // Create card containers for tv/movie banner tiles
+    console.log(mediaData.vote_average);
     const cardContainer = document.createElement('div');
     cardContainer.className = 'card';
     if (type === 'movie') {
         cardContainer.append(
             createPosterImageLink(mediaData.poster_path, mediaData.id, mediaData.original_title, type),
-            createMediaTileInformation(mediaData.original_title, mediaData.release_date)
+            createMediaTileInformation(mediaData.original_title, mediaData.vote_average)
         );
     } else if (type === 'show') {
         cardContainer.append(
             createPosterImageLink(mediaData.poster_path, mediaData.id, mediaData.name, type),
-            createMediaTileInformation(mediaData.name, mediaData.first_air_date)
+            createMediaTileInformation(mediaData.name, mediaData.vote_average)
         );
     }
 
@@ -35,15 +36,22 @@ function createPosterImageLink(poster, id, title, type) {
     return anchor;
 }
 
-function createMediaTileInformation(title, text) {
+function createMediaTileInformation(title, rating) {
     // Create card information for original_tile and release_date API values to fill tiles
     const mediaBodyTitle = document.createElement('h5');
     mediaBodyTitle.className = 'card-title';
     mediaBodyTitle.appendChild(document.createTextNode(title));
 
+    const starIcon = document.createElement('i');
+    starIcon.className = 'fas fa-star text-primary';
+    const average = document.createTextNode(` ${(parseFloat(rating).toFixed(1))} / 10`);
+
     const mediaBodyText = document.createElement('small');
     mediaBodyText.className = 'text-muted';
-    mediaBodyText.appendChild(document.createTextNode(`Release: ${text}`));
+    mediaBodyText.append(
+        starIcon,
+        average
+    );
 
     const mediaBodyTextContainer = document.createElement('p');
     mediaBodyTextContainer.className = 'card-text';
@@ -52,7 +60,7 @@ function createMediaTileInformation(title, text) {
     // Create card body container and append the card information
     const mediaTileContainer = document.createElement('div');
     mediaTileContainer.className = 'card-body';
-    mediaTileContainer.append(mediaBodyTitle, mediaBodyTextContainer);
+    mediaTileContainer.append(mediaBodyTextContainer, mediaBodyTitle);
 
     return mediaTileContainer;
 }
